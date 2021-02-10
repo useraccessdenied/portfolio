c91352eec09d311df7312c482622599939080ff6
Kapil Singaria <kpl.singaria69@gmail.com>
2021-02-11 00:13:05 +0530

    Initial basic layout ready

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # My Portfolio
+
+Visit: [kapilsingaria.ml](https://kapilsingaria.ml)
+My Blog: [blog.kapilsingaria.ml](https://blog.kapilsingaria.ml)
+
 Here lies my works!
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,24 @@
 module.exports = {
-  purge: ['./**/*.html',],
+  purge: ['./**/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          10: '#212121',
+          9: '#333333',
+          8: '#454545',
+          7: '#777777',
+          5: '#454545',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
   corePlugins: {
-    float: false
+    float: false,
   },
-}
+};
